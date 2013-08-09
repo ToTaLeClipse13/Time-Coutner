4ac6c7692277d233d6ba3309b7f95ad797dc643c
ToTaLeClipse <chriskj3@gmail.com>
2013-08-09 10:03:03 -0400

    0.0.2 Build
    Added Network Requirements

--- timeCounter_common/timeCounter/lib/Reference.java
+++ timeCounter_common/timeCounter/lib/Reference.java
@@ -3,6 +3,6 @@ package timeCounter.lib;
 public class Reference {
 	public static final String MOD_ID="Time Counter";
 	public static final String MOD_NAME="Time Counter";
-	public static final String VERSION="0.0.1";
+	public static final String VERSION="0.0.2";
 		
 }
--- timeCounter_common/timeCounter/timeCounter.java
+++ timeCounter_common/timeCounter/timeCounter.java
@@ -5,9 +5,10 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = Reference.MOD_ID, name=Reference.MOD_NAME,version=Reference.VERSION)
-
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
 
 public class timeCounter {
 	@EventHandler
