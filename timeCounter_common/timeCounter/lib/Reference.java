693e330df4262a9d14ef16808e5fd813bf6bd09a
ToTaLeClipse <chriskj3@gmail.com>
2013-08-09 09:56:34 -0400

    First Version

---
+++ timeCounter_common/timeCounter/lib/Reference.java
@@ -0,0 +1,8 @@
+package timeCounter.lib;
+
+public class Reference {
+	public static final String MOD_ID="Time Counter";
+	public static final String MOD_NAME="Time Counter";
+	public static final String VERSION="0.0.1";
+		
+}
---
+++ timeCounter_common/timeCounter/timeCounter.java
@@ -0,0 +1,26 @@
+package timeCounter;
+import timeCounter.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name=Reference.MOD_NAME,version=Reference.VERSION)
+
+
+public class timeCounter {
+	@EventHandler
+	public void load(FMLPreInitializationEvent event){
+		
+	}
+	@EventHandler
+	public void load(FMLInitializationEvent event){
+		
+	}
+	@EventHandler
+	public void load(FMLPostInitializationEvent event){
+		
+	}
+}
+ 
